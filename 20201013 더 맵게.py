569ac792e8b5cd7f1f2a92db75cea55a3d45deb3
HaedeunH <HaedeunH@users.noreply.github.com>
2020-10-14 15:04:41 +0900

    Update 20201013 더 맵게.py

--- 20201013 더 맵게.py
+++ 20201013 더 맵게.py
@@ -1,26 +1,22 @@
 import heapq
 
-def solutions(s,k):
-    count=0 
-    while True:
-        scoville=heapq.heapify(s)
-        if scoville[0]>k:
+def solution(s,k):
+    count=0
+    scoville=[]
+    for i in s:
+        heapq.heappush(scoville,i)
+    while(True):
+        if scoville[0]>=k:
             break
         elif len(scoville)==1:
             return -1
         else:
             count+=1
             heapq.heappush(scoville,heapq.heappop(scoville)+(heapq.heappop(scoville)*2))
-    return print(count,scoville)
+            return count
+            
+#append나 min pop 대신 힙을 이용하면 효율성이 좋다는점
 
-def solution(s,k):
-    count=0
-    scoville=[]
-    for i in s:
-        heapq.heappush(scoville,s)
-        print(scoville)
-
-    print(scoville,type(scoville))
             
 
 
