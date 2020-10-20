29a2428718716d027fe5383d48ac846532b14b58
HaedeunH <HaedeunH@users.noreply.github.com>
2020-10-20 22:35:35 +0900

    Update 20201016 위장.py

--- 20201016 위장.py
+++ 20201016 위장.py
@@ -1,16 +1,19 @@
 import collections
 
-list=[['yellow_hat', 'headgear'], ['blue_sunglasses', 'eyewear'], ['green_turban', 'headgear']]
-a=[]
-for x,y in list:
-    a.append(y)
-    print(a)
+def solution(clothes):
+    a=[]
+    b=[]
+    result=1
 
-total=collections.Counter(a)
+    for x,y in clothes: 
+        a.append(y)
 
+    total=collections.Counter(a)
+    color=total.values()
 
-# a리스트에 list[][i]의 종류를 모아둠
-#이걸 카운트해서 딕셔너리 형식으로 만들어
-#value값을 이용하든 아님 리스트에 [2,1]의 값을 만들든 해서
-#해당 리스트[i]+1 해버리고
-#리스트 내 값들을 다 더해버리고 -1 한걸 리턴하기 
+    for i in color: 
+        b.append(i)
+
+    for i in range(len(b)):
+        result*=(b[i]+1)
+    return result-1
