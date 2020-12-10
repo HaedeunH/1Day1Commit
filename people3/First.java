cb040dc89e5b8921557b490d37ac08816b72f386
HaedeunH <HaedeunH@users.noreply.github.com>
2020-12-10 15:07:10 +0900

    Create First.java

---
+++ people3/First.java
@@ -0,0 +1,8 @@
+
+public class First{
+    public static void main(String[] args){
+        System.out.print("Hello world");
+        System.out.print("\tOri");
+        System.out.print("\tclassroom");
+}
+}
