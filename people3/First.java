2e05133cefd335d9787274ddd359fffd0f9b301c
Chee285 <kxodud10054@gmail.com>
2020-12-10 16:33:06 +0900

    Delete First.java

--- people3/First.java
+++
@@ -1,8 +0,0 @@
-
-public class First{
-    public static void main(String[] args){
-        System.out.print("Hello world");
-        System.out.print("\tOri");
-        System.out.print("\tclassroom");
-}
-}
