b7fd9d6ee55563b169143efc86d7020572d6f797
wooilds1 <wooilds1@koreatech.ac.kr>
2020-12-11 13:45:19 +0900

    Add files via upload

---
+++ 12943 유민호 콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {  
+    public int solution(long num) {
+        return doCollatz(num);
+    }
+    
+    private static int doCollatz(long num){
+        for(int count=0;count<500;count++){
+            if(num!=1){
+                num = num%2==0?num/2:num*3+1;
+            }
+            else{
+                return count;
+            }
+        }
+        return -1;
+    }
+}
