a3d7fc06a313c620db4c0b743d0fd5f192b43ac7
MIN HO <wooilds1@koreatech.ac.kr>
2020-12-14 11:04:35 +0900

    Update 12921 유민호 소수 찾기.java

--- YOO MIN HO/12921 유민호 소수 찾기.java
+++ YOO MIN HO/12921 유민호 소수 찾기.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 // https://programmers.co.kr/learn/courses/30/lessons/12921#
 // ko.wikipedia.org/wiki/에라토스테네스의_체
-// 동적배열 이용시 효율성 테스트 통과못함
+// 동적배열 ArrayList 이용시 효율성 테스트 통과못함
 
 class Solution {
     public int solution(int n) {
