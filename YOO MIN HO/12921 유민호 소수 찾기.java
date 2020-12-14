b7a1591bc24919ba98ecf7574ad0f930dc7c527a
MIN HO <wooilds1@koreatech.ac.kr>
2020-12-14 11:11:04 +0900

    Update 12921 유민호 소수 찾기.java

--- YOO MIN HO/12921 유민호 소수 찾기.java
+++ YOO MIN HO/12921 유민호 소수 찾기.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 // https://programmers.co.kr/learn/courses/30/lessons/12921#
 // ko.wikipedia.org/wiki/에라토스테네스의_체
-// 동적배열 ArrayList 이용시 효율성 테스트 통과못함
 
 class Solution {
     public int solution(int n) {
         int answer = 0;
-		boolean[] primeList = new boolean[1000001];
+		boolean[] primeList = new boolean[n+1];
 		for(int i=2;i<=n;i++) {
 			primeList[i] = true;
 		}
